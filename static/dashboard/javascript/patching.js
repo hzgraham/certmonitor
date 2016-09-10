0d7802380354ed656a52a6fc18b00cf7eeea04c1
Henry Graham <hgraham@redhat.com>
2016-09-10 12:27:26 -0400

    adding static files to root directory

---
+++ static/dashboard/javascript/patching.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var $rows = $('#list-servers tr');
+    $('#input-server-list').keyup(function() {
+	var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+	$rows.show().filter(function() {
+	    var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+	    return !~text.indexOf(val);
+	}).hide();
+    });
+});
