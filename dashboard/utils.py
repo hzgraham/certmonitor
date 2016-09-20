a9dcf8d5101d37143701325f214b9d5f7388ee0f
Henry Graham <hgraham@redhat.com>
2016-09-19 23:29:46 -0400

    added a function sniQuery to utils.py that currently would return the subject components of a cert presented on a remote service

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -1,4 +1,14 @@
-def handle_uploaded_file(f):
-    with open('dashboard/test.txt', 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
+import OpenSSL
+
+class certTasks():
+    def handle_uploaded_file(f):
+        with open('dashboard/test.txt', 'wb+') as destination:
+            for chunk in f.chunks():
+                destination.write(chunk)
+
+    def sniQuery(servicename, port):
+        # produces a certificate DER hash
+        cert = ssl.get_server_certificate((servicename,port))
+        # returns the components of the subject line
+        x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+        x509.get_subject().get_components()
